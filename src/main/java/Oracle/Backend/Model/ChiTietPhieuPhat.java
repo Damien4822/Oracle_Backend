18baf73507aef14e5e7557ef19faae8191becd9e
Damien4822 <114857862+Damien4822@users.noreply.github.com>
2024-04-03 00:46:34 +0700

    making models

---
+++ src/main/java/Oracle/Backend/Model/ChiTietPhieuPhat.java
@@ -0,0 +1,19 @@
+package Oracle.Backend.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ChiTietPhieuPhat")
+public class ChiTietPhieuPhat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MaCTPP")
+    private int id;
+    private int ThanhTien;
+}
