da1d502f8e1903d4de1c4cde99e8af181d2d45cc
Damien4822 <114857862+Damien4822@users.noreply.github.com>
2024-04-04 01:24:51 +0700

    making repositories

--- src/main/java/Oracle/Backend/Repository/ChiTietPhieuPhatRepository.java
+++ src/main/java/Oracle/Backend/Repository/ChiTietPhieuPhatRepository.java
@@ -1,8 +1,9 @@
 package Oracle.Backend.Repository;
 
+import Oracle.Backend.Model.ChiTietPhieuPhat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChiTietPhieuPhatRepository extends JpaRepository<ChiTietPhieuPhatRepository,Integer> {
+public interface ChiTietPhieuPhatRepository extends JpaRepository<ChiTietPhieuPhat,Integer> {
 }
--- src/main/java/Oracle/Backend/Repository/PhanLoaiDocGiaRepository.java
+++ src/main/java/Oracle/Backend/Repository/PhanLoaiDocGiaRepository.java
@@ -1,8 +1,9 @@
 package Oracle.Backend.Repository;
 
+import Oracle.Backend.Model.PhanLoaiDocGia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PhanLoaiDocGiaRepository extends JpaRepository<PhanLoaiDocGiaRepository,Integer> {
+public interface PhanLoaiDocGiaRepository extends JpaRepository<PhanLoaiDocGia,Integer> {
 }
